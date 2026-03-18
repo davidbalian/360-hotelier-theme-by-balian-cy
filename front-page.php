ac3862b236386ff740cccdf44d162d577cdceacc
David Balyan <davidbalyan9@gmail.com>
2026-03-18 17:11:01 +0200

    refactor: Rearrange front page sections to improve content flow by adding 'why-choose', 'results', and 'approach' sections while removing duplicates for better layout coherence

--- front-page.php
+++ front-page.php
@@ -12,12 +12,12 @@ get_header(); ?>
 <main id="main" class="site-main front-page-main">
 
     <?php get_template_part( 'template-parts/front-page/section', 'hero' ); ?>
-    <?php get_template_part( 'template-parts/front-page/section', 'why-choose' ); ?>
     <?php get_template_part( 'template-parts/front-page/section', 'services-overview' ); ?>
+    <?php get_template_part( 'template-parts/front-page/section', 'why-choose' ); ?>
+    <?php get_template_part( 'template-parts/front-page/section', 'results' ); ?>
+    <?php get_template_part( 'template-parts/front-page/section', 'approach' ); ?>
     <?php get_template_part( 'template-parts/front-page/section', 'founder' ); ?>
     <?php get_template_part( 'template-parts/front-page/section', 'featured-banner' ); ?>
-    <?php get_template_part( 'template-parts/front-page/section', 'approach' ); ?>
-    <?php get_template_part( 'template-parts/front-page/section', 'results' ); ?>
     <?php get_template_part( 'template-parts/front-page/section', 'contact' ); ?>
 
 </main>
