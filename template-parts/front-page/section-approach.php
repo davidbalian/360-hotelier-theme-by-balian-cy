317797b8dcab7412e8ac64f5fc01094904a88527
David Balyan <davidbalyan9@gmail.com>
2026-03-30 12:30:31 +0300

    feat: Add image support for approach steps on front page, enhancing visual presentation and user engagement

--- template-parts/front-page/section-approach.php
+++ template-parts/front-page/section-approach.php
@@ -19,7 +19,21 @@ $hpage = (int) get_queried_object_id();
         <div class="front-approach__bento">
 
             <?php for ( $i = 1; $i <= 4; $i++ ) : ?>
+            <?php
+            $approach_step_img = Hotelier_Page_Content::get_image_url( $hpage, $hctx, 'approach_' . $i . '_img' );
+            ?>
             <div class="front-approach__bento-card card-border fade-in fade-in-delay-<?php echo esc_attr( (string) $i ); ?>">
+                <?php if ( $approach_step_img !== '' ) : ?>
+                <div class="front-approach__bento-card-thumb">
+                    <img
+                        src="<?php echo esc_url( $approach_step_img ); ?>"
+                        alt="<?php echo esc_attr( Hotelier_Page_Content::get_text( $hpage, $hctx, 'approach_' . $i . '_img_alt' ) ); ?>"
+                        loading="lazy"
+                        decoding="async"
+                        sizes="(max-width: 768px) calc(100vw - 64px), (max-width: 1200px) 45vw, 560px"
+                    />
+                </div>
+                <?php endif; ?>
                 <span class="front-approach__step-number"><?php echo esc_html( str_pad( (string) $i, 2, '0', STR_PAD_LEFT ) ); ?></span>
                 <h3 class="front-approach__step-title"><?php echo esc_html( Hotelier_Page_Content::get_text( $hpage, $hctx, 'approach_' . $i . '_title' ) ); ?></h3>
                 <p class="front-approach__step-text text-body"><?php echo esc_html( Hotelier_Page_Content::get_text( $hpage, $hctx, 'approach_' . $i . '_text' ) ); ?></p>
